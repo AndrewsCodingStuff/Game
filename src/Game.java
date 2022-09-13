f68dc8fce74b57414cad61493501616166827ac9
Andrew <andrewgtz099@gmail.com>
2022-09-13 10:37:18 -0700

    Submit

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+	private int money = 0;
+	public Game() {
+		money = 1000;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+
+}
---
+++ src/gameTester.java
@@ -0,0 +1,9 @@
+
+public class gameTester {
+
+	public static void main(String[] args) {
+		Game big = new Game();
+		System.out.println(big.getMoney());
+	}
+
+}
